21123b9c6cdda7335daa57fa0d2a1d078da5575b
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-21 18:17:11 -0600

    Login - imagen  izquierda

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Header from '../../components/Header/auth';
 import Error from '../../error/error';
+import styles from './css/auth.module.css'
 
 const Login = () => {
     return (
@@ -14,6 +15,23 @@ const Login = () => {
                 <Error>
                     <Header />
                 </Error>
+
+                <div className={`${styles.container}`} >
+                    <div className={`${styles.wrapper}`} >
+
+                        <section className={`${styles.backgroundImage}`} >
+                            <div className={`${styles.welcomeContainer}`}>
+                                <h2>Welcome Back!</h2>
+                                <p>The herd awaits you</p>
+                            </div>
+                        </section>
+
+                        <section className={`${styles.loginRight}`} >
+                            .
+                        </section>
+
+                    </div>
+                </div>
 
             </motion.div>
         </AnimatePresence>
