0f31d8728a701f84357f200ce563fe876eee113d
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-25 19:40:56 -0600

    Retiro - Botón y Nota de descargo de responsabilidad

--- src/components/modal/wallet-withdraw/withdraw.js
+++ src/components/modal/wallet-withdraw/withdraw.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styles from '../css/wallet.module.css';
+import styles from '../css/withdraw/withdraw.module.css';
 import CoinNetwork from '../../coin-network/withdraw/coin-network';
 
 const WithdrawWallet = (props) => {
@@ -8,6 +8,8 @@ const WithdrawWallet = (props) => {
     useEffect(() => {
         document.title = 'Withdraw | GOLDENTICKETS.CLUB';
     }, []); //eslint-disable-line
+
+    const isInvalid = parseFloat(props.amount) <= 0 && props.currency === '';
 
     return (
         <>
@@ -35,16 +37,47 @@ const WithdrawWallet = (props) => {
 
             <div className={`${styles.divider}`}></div>
 
-            {/* GENERATE & DISPLAY DEPOSIT ADDRESS */}
+            {/* GENERATE & DISPLAY WITHDRAW ADDRESS */}
 
             <div className={`${styles.generateContainer}`} >
                 <div className={`${styles.generateWrapper}`} >
 
                     <span className={`${styles.generateAddress}`}>
+                        <div className={`${styles.qr}`} >
+                            <div className={`${styles.paymentWarninigNote}`} >
 
+                                <h3>Note:</h3>
+
+                                <ul className={`${styles.paymentWarning}`} >
+                                    <li>• We currently support withdraw only to <b className='uppercase'> USDT (BEP20)</b> Wallets</li>
+                                    <li>• Please ensure that you do NOT input a TRC, ETH address as a USDT (TRC20)
+                                        withdrawal address wallets
+                                    </li>
+                                    <li>• Please do NOT withdraw via cross-chain transfer</li>
+                                    <li>• in case you do NOT follow the above withdrawal rules, YOUR FUND WILL BE LOST</li>
+                                </ul>
+                            </div>
+
+                            <div className={`${styles.withdrawContainer}`}>
+
+                                <button
+                                    type='button'
+                                    className={`${styles.button} ${isInvalid && 'cursor-not-allowed'}`}
+                                    onClick={() => console.log('withdraw')}
+                                    disabled={isInvalid}
+                                >
+                                    Withdraw
+                                </button>
+
+                            <p>Withdrawal fee  0.16876 USD</p>
+
+                            </div>
+                        </div>
 
 
                     </span>
+
+
                 </div>
             </div>
         </>
