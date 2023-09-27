dbf17d112a01ef2577246de01ec3bd37b5d287f5
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-27 11:41:26 -0600

    FIx - No permitir compra de ticket sí el usuario no tiene saldo suficiente & Registro de compra de tickets

--- functions/utils/scheduled-functions.js
+++ functions/utils/scheduled-functions.js
@@ -2,55 +2,9 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
-
-exports.BuyTickets = functions.https.onCall(async (data, context) => {
-
-  const { selectedNumbers, userId, costoTicket } = data;
-
-  try {
-    // Verifica que el usuario esté autenticado
-    if (!context.auth) {
-      throw new Error('Usuario no autenticado');
-    }
-
-    const estadoLoteriaRef = db.collection('loteria').doc('estado');
-
-    // Inicializa un array para almacenar las compras
-    const compras = [];
-
-    // Recorre los números de tickets y agrega cada compra al array
-    for (const numeroTicket of selectedNumbers) {
-      // Agrega la compra del ticket al array
-      compras.push({
-        numeroTicket: numeroTicket,
-        userId: userId,
-        costoTicket: costoTicket,
-      });
-    }
-
-    // Agrega el array de compras a la subcolección "compras" en Firestore
-    await estadoLoteriaRef.collection('compras').add({ compras });
-
-    // Calcula el premio acumulado sumando el costo total de los tickets
-    const costoTotalTickets = parseFloat(costoTicket) * selectedNumbers.length;
-    const estadoActual = (await estadoLoteriaRef.get()).data();
-    const premioAcumuladoActual = estadoActual.premioAcumulado || 0;
-    const nuevoPremioAcumulado = premioAcumuladoActual + costoTotalTickets;
-
-    // Actualiza el premio acumulado y el número ganador en Firestore
-    await estadoLoteriaRef.update({
-      premioAcumulado: nuevoPremioAcumulado,
-    });
-
-    return { message: 'Compra exitosa' };
-  } catch (error) {
-    return { error: error.message };
-  }
-});
-
 exports.BuyTicketsV2 = functions.https.onCall(async (data, context) => {
 
-  const { selectedNumbers, userId, costoTicket } = data;
+  const { selectedNumbers, userId, costoTicket, totalPrice } = data;
 
   try {
     // Verifica que el usuario esté autenticado
@@ -58,37 +12,62 @@ exports.BuyTicketsV2 = functions.https.onCall(async (data, context) => {
       throw new Error('Usuario no autenticado');
     }
 
-    const estadoLoteriaRef = db.collection('loteria').doc('estado');
+    const userRef = db.collection('users').doc(userId);
+    const userData = await userRef.get();
 
-    // Inicializa un array para almacenar las compras
-    const comprasPromises = [];
+    // Condición para conocer sí el saldo del usuario es mayor o igual que el costo total del ticket + fee
+    const doesUserHaveSufficientBalance = userData?.data()?.Balance >= totalPrice && totalPrice <= userData?.data()?.Balance;
 
-    // Recorre los números de tickets y agrega cada compra como un documento independiente
-    for (const numeroTicket of selectedNumbers) {
-      const compraData = {
-        numeroTicket: numeroTicket,
-        userId: userId,
-        costoTicket: costoTicket,
-      };
+    if (userData.exists) { // Verificamos que el usuario exista
+      if (doesUserHaveSufficientBalance) { // Verificamos que el saldo del usuario sea mayor o igual al total del costo de los tickets 
 
-      // Agrega la compra del ticket como un documento en la subcolección "compras"
-      const compraPromise = estadoLoteriaRef.collection('compras').add(compraData);
-      comprasPromises.push(compraPromise);
+        const estadoLoteriaRef = db.collection('loteria').doc('estado');
+
+        // Inicializa un array para almacenar las compras
+        const comprasPromises = [];
+
+        // Recorre los números de tickets y agrega cada compra como un documento independiente
+        for (const numeroTicket of selectedNumbers) {
+          const compraData = {
+            numeroTicket: numeroTicket,
+            userId: userId,
+            costoTicket: costoTicket,
+          };
+
+          // Agrega la compra del ticket como un documento en la subcolección "compras"
+          const compraPromise = estadoLoteriaRef.collection('compras').add(compraData);
+          // Registro de cada ticket 
+          const saveTickets = db.collection('vault-tickets').add(compraData);
+          comprasPromises.push(compraPromise);
+        }
+
+        // Espera a que se completen todas las compras
+        await Promise.all(comprasPromises);
+
+        // Calcula el premio acumulado sumando el costo total de los tickets
+        const costoTotalTickets = parseFloat(costoTicket) * selectedNumbers.length;
+        const estadoActual = (await estadoLoteriaRef.get()).data();
+        const premioAcumuladoActual = estadoActual.premioAcumulado || 0;
+        const nuevoPremioAcumulado = premioAcumuladoActual + costoTotalTickets;
+
+        // Actualiza el premio acumulado y el número ganador en Firestore
+        await estadoLoteriaRef.update({
+          premioAcumulado: nuevoPremioAcumulado,
+        });
+
+
+
+        // Actaliza el documento del usuario que compro los tickets
+        // restando el costo del ticket del saldo del usuario
+        await userRef.update({
+          Balance: userData?.data()?.Balance - totalPrice,
+        });
+      } else {
+        console.log('doesnt have sufficient balance!')
+      }
+    } else {
+      console.log('No such document!')
     }
-
-    // Espera a que se completen todas las compras
-    await Promise.all(comprasPromises);
-
-    // Calcula el premio acumulado sumando el costo total de los tickets
-    const costoTotalTickets = parseFloat(costoTicket) * selectedNumbers.length;
-    const estadoActual = (await estadoLoteriaRef.get()).data();
-    const premioAcumuladoActual = estadoActual.premioAcumulado || 0;
-    const nuevoPremioAcumulado = premioAcumuladoActual + costoTotalTickets;
-
-    // Actualiza el premio acumulado y el número ganador en Firestore
-    await estadoLoteriaRef.update({
-      premioAcumulado: nuevoPremioAcumulado,
-    });
 
     return { message: 'Compra exitosa' };
   } catch (error) {
