6ee1af6b37cb0b3ac0c111e204c187e03d1185b1
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-26 20:10:13 -0600

    Rutas - Compra de tickets de Loteria

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,9 +3,12 @@ export const LOGIN = '/login';
 export const SIGNUP = '/register';
 export const RECOVER_PASSWORD = '/reset-password';
 
+// Dashboard
 export const DASHBOARD = '/home';
-export const BUY_TICKETS = '/buy-tickets';
-export const DRAW = '/winner-draw';
+
+// Room 1 - Lottery Prize: $100 USD
+export const BUY_TICKETS = '/draw/prize-1/buy-tickets';
+export const DRAW = '/draw/prize-1/winner-draw';
 
 
 export const ADMIN_DASHBOARD = '/panel/admin-dashboard';
