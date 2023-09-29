3ad0eb2836b1ac15775ba93f446eed40c0356054
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-29 13:18:56 -0600

    Feature - loader de loteria

---
+++ src/components/landing/loader/lotteries-loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../css/lotteries-loader.module.css'
+
+const LotteriesLoader = () => {
+    return (
+        <div className={`${styles.lotteryHistoryWrapper} animate-pulse w-full h-36`}  >
+
+
+        </div>
+    )
+}
+
+export default LotteriesLoader
