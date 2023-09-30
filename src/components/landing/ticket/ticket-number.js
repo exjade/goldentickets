83a03f8fb120b5dfb1208290146d00519347f52d
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-30 06:54:04 -0600

    Fix - limpiar código que no se utiliza

--- src/components/landing/ticket/ticket-number.js
+++ src/components/landing/ticket/ticket-number.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 import styles from './css/ticket-number.module.css';
 
-const TicketNumber = (props) => {
+const TicketNumber = () => {
 
 
   return (
