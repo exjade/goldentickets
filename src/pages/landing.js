0c98858b4967dcda8309a5506a2fc9345cda685f
Adrian Zamora <adrianzamora10@outlook.com>
2023-09-30 22:14:03 -0600

    Header - Condicional de render de componente Header por rol de usuario

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Error from '../error/error';
-import Header from '../components/Header/landing';
+import Header from '../components/Header/dashboard';
+import LandingHeader from '../components/Header/landing';
 import LotteriesTimeline from '../components/landing';
 import Timeline from '../components/timeline';
 import useModal from '../hooks/use-modal';
+import useAuthListener from '../hooks/use-auth-listener';
 
 const Landing = () => {
   const { open, openModal, closeModal } = useModal()
+  const { user } = useAuthListener()
 
   return (
     <AnimatePresence>
@@ -16,22 +19,32 @@ const Landing = () => {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
       >
+
+        {
+          user?.uid === undefined || user?.uid === null ? (
+            <Error>
+              <LandingHeader />
+            </Error>
+          ) :
+            (
+              <Error>
+                <Header
+                  openModal={openModal}
+                />
+              </Error>
+            )
+        }
+
         <Error>
-          <Header 
-            openModal={openModal}
+          <Timeline
+            open={open}
+            closeModal={closeModal}
           />
         </Error>
 
         <Error>
-            <Timeline
-              open={open}
-              closeModal={closeModal}
-            />
-          </Error>
-
-          <Error>
-            <LotteriesTimeline />
-          </Error>
+          <LotteriesTimeline />
+        </Error>
 
       </motion.div>
     </AnimatePresence>
