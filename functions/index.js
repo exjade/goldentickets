dbe7d3e4794994848a0c89fbbc36dfbec921df27
Adrian Zamora <adrianzamora10@outlook.com>
2023-10-01 01:53:59 -0600

    Fix - Sí hay un ganador: reset del premioAcumulado, y númeroNoGanador

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,7 @@ const {
 
 const {
     sorteoAutomaticoDolarHoraV3,
+    sorteoAutomaticoDolarHoraV4,
 } = require('./utils/scheduled-functions/Sorteo-dolar/sorteoAutomatico');
 const {
     DeleteReservedTickets,
@@ -25,6 +26,7 @@ const db = admin.firestore();
 exports.BuyTicketsV3 = BuyTicketsV3;
 exports.BuyTicketsBETAV14 = BuyTicketsBETAV14;
 exports.sorteoAutomaticoDolarHoraV3 = sorteoAutomaticoDolarHoraV3;
+exports.sorteoAutomaticoDolarHoraV4 = sorteoAutomaticoDolarHoraV4;
 exports.DeleteReservedTickets = DeleteReservedTickets;
 
 
