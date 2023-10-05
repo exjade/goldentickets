d51962d6a320fc2d377104a27dd286f9d2b68c1f
Adrian Zamora <adrianzamora10@outlook.com>
2023-10-04 19:57:23 -0600

    NEW - elminar tickets de loteria semanal

--- functions/utils/scheduled-functions/delete/delete-bookingtickets.js
+++ functions/utils/scheduled-functions/delete/delete-bookingtickets.js
@@ -24,3 +24,27 @@ exports.DeleteReservedTickets = functions.pubsub.schedule('0 * * * *').timeZone(
             return null;
         });
 });
+
+
+exports.WeeklyDeleteReservedTickets = functions.pubsub.schedule('0 20 * * FRI').timeZone('America/Mexico_City').onRun((context) => {
+    const collectionRef = db.collection('weekly-booking-tickets');
+
+    // Obtén todos los documentos de la colección
+    return collectionRef.get()
+        .then(snapshot => {
+            // Elimina cada documento
+            const promises = [];
+            snapshot.forEach(doc => {
+                promises.push(doc.ref.delete());
+            });
+            return Promise.all(promises);
+        })
+        .then(() => {
+            console.log('Documentos eliminados correctamente.');
+            return null;
+        })
+        .catch(error => {
+            console.error('Error al eliminar documentos:', error);
+            return null;
+        });
+});
