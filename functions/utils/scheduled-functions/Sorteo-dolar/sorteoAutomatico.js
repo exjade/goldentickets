30d156346893402a51a003cb04a1f2e170dcdf5a
Adrian Zamora <adrianzamora10@outlook.com>
2023-10-04 19:57:55 -0600

    NEW - nuevo sorteo cada día viernes

--- functions/utils/scheduled-functions/Sorteo-dolar/sorteoAutomatico.js
+++ functions/utils/scheduled-functions/Sorteo-dolar/sorteoAutomatico.js
@@ -237,3 +237,128 @@ exports.sorteoAutomaticoDolarHoraV6 = functions.pubsub.schedule('0 * * * *').tim
         return null;
     }
 });
+
+// ========================== ============================================= ========================= //
+// ========================== LOTERIA DE $1000 PREMIO, COSTO DE $15 DOLARES ========================= //
+// ========================== ============================================= ========================= //
+
+
+
+exports.WeeklySorteoAutomaticoDolarHoraV2 = functions.pubsub.schedule('0 20 * * FRI').timeZone('America/Mexico_City').onRun(async (context) => {
+    try {
+        // 1. Genera un número aleatorio como el número ganador
+        const nuevoNumeroGanador = Math.floor(Math.random() * 100) + 1;
+
+        // 2. Consulta todas las compras que coinciden con el número ganador
+        const comprasSnapshot = await db.collection('loteria').doc('weekly-prize').collection('compras')
+            .where('numeroTicket', '==', nuevoNumeroGanador)
+            .get();
+
+        // 3. Obtiene el premio acumulado sumando el costo de los tickets
+        const premioRef = db.collection('loteria').doc('weekly-prize');
+        const premioData = await premioRef.get();
+
+        const estadoLoteriaRef = db.collection('loteria').doc('weekly-prize');
+
+        if (!comprasSnapshot.empty) {
+            // Si hay compras con el número ganador, otorga el premio acumulado al usuario
+            const ganadorId = comprasSnapshot.docs[0].data().userId;
+
+            // 5. Entregar el premio al ganador
+            const userRef = db.collection('users').doc(ganadorId);
+            const userData = await userRef.get();
+
+            await userRef.update({
+                Balance: userData?.data()?.Balance + premioData?.data()?.premioAcumulado,
+                Withdrawal: userData?.data()?.Withdrawal + premioData?.data()?.premioAcumulado,
+            });
+            const id = Math.floor(Math.random() * 450343) + 45;
+
+
+            const informaciónGanador = {
+                username: userData?.data()?.username,
+                userId: userData?.data()?.userId,
+                emailAddress: userData?.data()?.emailAddress,
+                numeroGanador: nuevoNumeroGanador,
+                premioAcumulado: premioData?.data()?.premioAcumulado,
+                date: Date.now(),
+                id: id,
+            }
+
+            const informaciónSorteo = {
+                username: userData?.data()?.username,
+                userId: userData?.data()?.userId,
+                emailAddress: userData?.data()?.emailAddress,
+                numeroGanador: nuevoNumeroGanador,
+                premioAcumulado: premioData?.data()?.premioAcumulado,
+                date: Date.now(),
+                id: id,
+            }
+
+            // Actualiza el premio acumulado y el número ganador en Firestore
+            await estadoLoteriaRef.update({
+                numeroGanador: nuevoNumeroGanador,
+                numeroNoGanador: 0,
+                premioAcumulado: 0,
+                premioEntregado: premioData?.data()?.premioAcumulado,
+                date: Date.now(),
+                ultimoGanador: {
+                    userId: userData?.data()?.userId,
+                    username: userData?.data()?.username,
+                }
+            });
+
+            // 9-10. Almacenar un registro de ganadores y úlimas loterias
+            const winnersLogs = db.collection('weekly-lottery-winners').add(informaciónGanador);
+            const lotteryLogs = db.collection('weekly-last-lotteries').add(informaciónSorteo);
+
+
+            // 12. Después del sorteo, elimina la colección "compras"
+            await db.collection('loteria').doc('weekly-prize').collection('compras').get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    doc.ref.delete();
+                });
+            });
+
+
+            // Notifica al ganador o realiza las acciones necesarias
+            console.log(`¡Tenemos un ganador! Usuario ID: ${ganadorId}`);
+        } else {
+            // Si nadie compró el número ganador, acumula el premio
+            // Actualiza solo el premio acumulado y el número ganador en Firestore
+            await estadoLoteriaRef.update({
+                numeroGanador: 0,
+                numeroNoGanador: nuevoNumeroGanador,
+                premioAcumulado: premioData?.data()?.premioAcumulado,
+                premioEntregado: 0,
+                date: Date.now(),
+            });
+
+            // 9-10. Almacenar un registro de las loterias acumuladas
+            const id = Math.floor(Math.random() * 450343) + 45;
+            const informaciónSorteo = {
+                numeroGanador: nuevoNumeroGanador,
+                premioAcumulado: premioData?.data()?.premioAcumulado,
+                date: Date.now(),
+                id: id,
+            }
+
+            const cumulativePrize = db.collection('weekly-accumulated-prize').add(informaciónSorteo);
+
+              // 12. Después del sorteo, elimina la colección "compras"
+              await db.collection('loteria').doc('weekly-prize').collection('compras').get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    doc.ref.delete();
+                });
+            });
+
+
+            console.log(`Nadie compró el número ganador. Premio acumulado: $${premioData?.data()?.premioAcumulado}`);
+        }
+
+        return null;
+    } catch (error) {
+        console.error('Error en el sorteo automático:', error);
+        return null;
+    }
+});
