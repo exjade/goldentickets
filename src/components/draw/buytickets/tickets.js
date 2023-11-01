e7a29664613cedbe1b6a116190444255e1ca63a0
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-01 17:47:05 -0600

    feat: Añade nuevo campo 'sellerCode' a la colección de 'booking-tickets'

--- src/components/draw/buytickets/tickets.js
+++ src/components/draw/buytickets/tickets.js
@@ -86,6 +86,7 @@ const Tickets = (props) => {
                                     disponible: true,
                                     fechaReserva: Date.now(),
                                     enProceso: true,
+                                    sellerCode: sellerCode,
                                 });
 
 
