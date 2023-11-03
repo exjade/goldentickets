1bb3adf879efe205dd9f14a3e70dd8206a4efba0
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-03 14:58:39 -0600

    feat: Crear componente 'SearchBar' y su archivo de estilos

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles/searchbar.module.css'
 
-const Searchbar = () => {
+const Searchbar = ({
+  title,
+}) => {
   return (
-    <div>Searchbar</div>
+    <div className={`${styles.container}`} >
+      <div className={`${styles.wrapper}`} >
+
+        <div className={`${styles.title}`} >
+          <h3>{title}</h3>
+        </div>
+        <div className={`${styles.searchbar}`} >
+          <input
+            type="search"
+            name="searchbar"
+            id="searchbar"
+            placeholder='Search' 
+            className='w-full mx-4 py-1 text-center outline-none'
+            />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
 export default Searchbar
+
+Searchbar.propTypes = {
+  title: PropTypes.string,
+}
