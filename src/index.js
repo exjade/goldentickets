6b19514157cb9f374db241f0d0542ebc478206e7
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-03 14:53:26 -0600

    refactor: Eliminar contenedor redundante del render en 'index.js'

--- src/index.js
+++ src/index.js
@@ -10,22 +10,21 @@ import reportWebVitals from './reportWebVitals';
 const root = document.getElementById('root');
 const rootElement = createRoot(root);
 rootElement.render(
-    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-        <App />
-    </FirebaseContext.Provider>
-    , document.getElementById('root')
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>
 );
 
 serviceWorkerRegistration.register({
-    onUpdate: async (registration) => {
-      if (registration && registration.waiting) {
-        await registration.unregister();
-        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-        window.location.reload();
-      }
-    },
-  });
+  onUpdate: async (registration) => {
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      window.location.reload();
+    }
+  },
+});
 
-  // If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
