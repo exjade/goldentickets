3c12692fe4ba5942497e25962c50e32f535d9ae1
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-02 22:57:41 -0600

    feat: Crear componente 'card-large'

---
+++ src/components/Card/card-large.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import styles from './styles/card-large.module.css';
+
+const CardLarge = ({
+    content
+}) => {
+    return (
+        <div className={`${styles.container}`} >
+            {content}
+        </div>
+    )
+}
+
+export default CardLarge
+
+CardLarge.propTypes = {
+    content: PropTypes.string.isRequired,
+};
