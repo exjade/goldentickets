15ef9c1aa973d624f83437e42b0c5ca14ce76f31
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-02 18:04:26 -0600

    fix: El menu desplegable del 'header/dashboard' no se mostraba en página de 'afiliados'

--- src/pages/afiliados/myreferrals.js
+++ src/pages/afiliados/myreferrals.js
@@ -6,12 +6,14 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { motion } from 'framer-motion'
 import useWeeklyTickets from '../../hooks/draw/weekly-prize/use-weeklyTickets';
 import { formatRelative } from 'date-fns'
+import FallBackLoader from '../../components/FallBackLoader';
 
 const MyReferrals = () => {
 
     const { user } = useUser()
     const { tickets: weeklyTickets } = useWeeklyTickets()
     const [code, setCode] = useState('')
+    const [loader, setLoader] = useState(false)
 
 
 
@@ -19,70 +21,84 @@ const MyReferrals = () => {
         const sellerId = user?.userId
 
         const result = getDailySellerCode(sellerId).then(result => {
-            setCode(result[0].dailyCode)
-        })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+            if (result === undefined) {
+                setLoader(true)
+            } else {
+                setCode(result[0].dailyCode)
+            }
+        }).catch(error => {
+            console.error('Error:', error);
+        });
+
+
+        setTimeout(() => {
+            setLoader(false)
+        }, [1000])
+
         return () => result
     }, [user])
 
     const filterTicketsWithSellerCode = weeklyTickets?.filter(ticket => ticket.sellerCode === code)
 
-    return (
-        <div className={`${styles.container}`} >
-            <div className={`${styles.wrapper}`} >
+    if (loader) {
+        return <FallBackLoader />
+    }
+    else {
+        return (
+            <div className={`${styles.container}`} >
+                <div className={`${styles.wrapper}`} >
 
-                <div className={`${styles.welcomeMessage}`} >
-                    <p>Bienvenido, {user?.emailAddress}</p>
-                </div>
+                    <div className={`${styles.welcomeMessage}`} >
+                        <p>Bienvenido, {user?.emailAddress}</p>
+                    </div>
 
-                <div className={`${styles.sellerCode}`} >
-                    {
-                        code === undefined || code === '' ?
-                            (
-                                <p>Código: loading... </p>
-                            ) :
-                            (
-                                <>
-                                    <p>Tú código de venta:</p>
-                                    < CopyToClipboard text={code}>
-                                        <motion.button
-                                            type='button'
-                                            className={`${styles.referralCode} lowercase font-semibold`}
-                                            whileTap={{ scale: 0.9 }}>
-                                            {code}
-                                        </motion.button>
-                                    </CopyToClipboard>
-                                </>
-                            )
-                    }
-                </div>
-
-                <div className={`${styles.WrapperTickets}`} >
-                    <div className={`${styles.ticket}`} >
-                        <p>Tickets vendidos de $15 USD</p>
-
+                    <div className={`${styles.sellerCode}`} >
                         {
-                            filterTicketsWithSellerCode?.map((ticket, index) => (
-                                <div
-                                    key={index}
-                                    className={`${styles.table}`}
-                                >
-                                    <p>code: {ticket.sellerCode}</p>
-                                    <p>costo: {ticket.costoTicket} USD</p>
-                                    <p>numero: {ticket.numeroTicket}</p>
-                                    <p>Loteria: {ticket.drawType}</p>
-                                    <p>Fecha:  {formatRelative(ticket.purchaseDate, new Date(), { addSuffix: true })}</p>
-                                </div>
-                            ))
+                            code === undefined || code === '' ?
+                                (
+                                    <p>Código: loading... </p>
+                                ) :
+                                (
+                                    <>
+                                        <p>Tú código de venta:</p>
+                                        < CopyToClipboard text={code}>
+                                            <motion.button
+                                                type='button'
+                                                className={`${styles.referralCode} lowercase font-semibold`}
+                                                whileTap={{ scale: 0.9 }}>
+                                                {code}
+                                            </motion.button>
+                                        </CopyToClipboard>
+                                    </>
+                                )
                         }
                     </div>
-                </div>
 
-            </div>
-        </div >
-    )
+                    <div className={`${styles.WrapperTickets}`} >
+                        <div className={`${styles.ticket}`} >
+                            <p>Tickets vendidos de $15 USD</p>
+
+                            {
+                                filterTicketsWithSellerCode?.map((ticket, index) => (
+                                    <div
+                                        key={index}
+                                        className={`${styles.table}`}
+                                    >
+                                        <p>code: {ticket.sellerCode}</p>
+                                        <p>costo: {ticket.costoTicket} USD</p>
+                                        <p>numero: {ticket.numeroTicket}</p>
+                                        <p>Loteria: {ticket.drawType}</p>
+                                        <p>Fecha:  {formatRelative(ticket.purchaseDate, new Date(), { addSuffix: true })}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                </div>
+            </div >
+        )
+    }
 }
 
 export default MyReferrals
