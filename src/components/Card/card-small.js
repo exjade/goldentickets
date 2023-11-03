709af4ca037e198debd70466fc017c2259161c3d
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-02 22:35:53 -0600

    feat: Nuevo componente 'card-small'

---
+++ src/components/Card/card-small.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles/card-small.module.css';
+
+const CardSmall = ({
+  title,
+  img,
+  alt,
+  price,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.wrapper}`} >
+        <img
+          src={img}
+          alt={alt}
+          className='object-contain w-16 h-16'
+        />
+        <div className={styles.article}>
+          <p className={styles['article-name']}>{title}</p>
+          <p className={styles['article-price']}>
+            {price}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default CardSmall;
+
+CardSmall.propTypes = {
+  title: PropTypes.string.isRequired,
+  img: PropTypes.string,
+  alt: PropTypes.string,
+  price: PropTypes.number,
+};
