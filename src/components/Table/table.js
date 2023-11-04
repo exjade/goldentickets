a3097d146ba163faf58ad1086ca26ab87522f3fd
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-03 20:44:47 -0600

    feat: Crear nuevo componente 'Table' junto con su hoja de estilos

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles/table.module.css';
+import { format } from 'date-fns'
 
-const Table = () => {
+const Table = ({
+  item,
+}) => {
+
+  const cadena = item.sellerCode;
+  const resultado = cadena.match(/-(.*)$/)[1];
+
   return (
-    <div>Table</div>
+    <div className={`${styles.container}`} >
+      <table className={`${styles.wrapper}`} >
+
+        <thead className={`${styles['table-header']}`}>
+          <tr className={`${styles['table-header-row']}`}>
+            <th>Fecha</th>
+            <th className='hidden md:inline'>id</th>
+            <th className='hidden md:inline'>Usuario</th>
+            <th>#</th>
+            <th>Costo</th>
+            <th>Comisión</th>
+          </tr>
+        </thead>
+
+        <tbody className={`${styles['table-body']}`}>
+          <tr className={`${styles['table-body-row']}`}>
+            <td>
+              {format(new Date(item.purchaseDate), 'dd/MM/yyyy')}
+            </td>
+            <td className='hidden md:inline'>
+              {item.id}
+            </td>
+            <td className='hidden md:inline'>
+              {item.username}
+            </td>
+            <td>
+              {item.numeroTicket}
+            </td>
+            <td>
+              {item.costoTicket}
+            </td>
+            <td>
+              {parseFloat(item.costoTicket * 0.2)?.toFixed(2)}
+            </td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
   )
 }
 
 export default Table
+
+Table.propTypes = {
+  item: PropTypes.object,
+}
+
+
+/* 
+4. **Historial de ventas** 
+    1. Fecha
+    2. Numero de ticket
+    3. Código de vendedor
+    4. Nombre de usuario
+    5. Id de ticket
+    6. Costo de ticket
+    7. Comisión por ticket
+*/
