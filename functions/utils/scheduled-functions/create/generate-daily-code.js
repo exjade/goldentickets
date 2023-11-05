999c43073194863ac83907e2315ced55d12eb849
Adrian Zamora <adrianzamora10@outlook.com>
2023-11-04 20:56:48 -0600

    Añadida función para generar códigos diarios individualizados En esta modificación, se implementó una función de Firebase en exports.generateIndidualSellerCodeV1 mediante functions.https.onRequest. La función maneja solicitudes HTTP, permitiendo la generación de códigos diarios únicos asociados a vendedores individuales.
    Detalles:
    
    La función extrae el sellerId de la solicitud HTTP y verifica su presencia.
    Se genera un código único diario mediante la función generateUniqueCode().
    El código se almacena en la colección 'dailyCodes' de Firestore con su correspondiente marca de tiempo.
    Se obtiene información del vendedor asociado al sellerId.
    Se crea un objeto userData con detalles relevantes del vendedor.
    El código generado se asocia al vendedor específico en la colección 'dailyCodesAssociations'.
    Se manejan respuestas HTTP para distintos escenarios, como éxito, error en la solicitud o problemas internos.

--- functions/utils/scheduled-functions/create/generate-daily-code.js
+++ functions/utils/scheduled-functions/create/generate-daily-code.js
@@ -48,6 +48,59 @@ exports.generateDailyCodeV5 = functions.https.onRequest(async (req, res) => {
     });
 });
 
+
+exports.generateIndidualSellerCodeV1 = functions.https.onRequest(async (req, res) => {
+    cors(req, res, async () => {
+        const sellerId = req.body.sellerId;  // Obtener el sellerId desde la solicitud
+
+        // Verificar si sellerId está presente
+        if (!sellerId) {
+            return res.status(400).send('Error: sellerId no proporcionado');
+        }
+
+        // Generar código único diario
+        const dailyCode = generateUniqueCode();
+
+        try {
+            // Guardar el código en Firestore
+            const dailyCodeRef = await admin.firestore().collection('dailyCodes').add({
+                code: dailyCode,
+                timestamp: Date.now(),
+            });
+
+            // Obtener información del vendedor
+            const sellerDoc = await admin.firestore().collection('sellers').doc(sellerId).get();
+            const sellerData = sellerDoc.data();
+
+            if (!sellerData) {
+                return res.status(404).send('Error: Vendedor no encontrado');
+            }
+
+            const userData = {
+                username: sellerData.username,
+                email: sellerData.emailAddress,
+                referralCode: sellerData.referral.referralCode,
+                rol: sellerData.rol,
+                date: Date.now()
+            };
+
+            // Asociar el código con el vendedor específico
+            await admin.firestore().collection('dailyCodesAssociations').add({
+                sellerId,
+                dailyCodeId: dailyCodeRef.id,
+                userData,
+                dailyCode
+            });
+
+            return res.status(200).send(`Código generado con éxito: ${dailyCode}`);
+        } catch (error) {
+            console.error('Error al generar el código:', error);
+            return res.status(500).send('Error interno al generar el código');
+        }
+    });
+});
+
+
 function generateUniqueCode() {
     // Implementa tu lógica para generar códigos únicos (puedes usar UUID o cualquier otra estrategia)
     // Aquí hay un ejemplo simple utilizando la fecha actual y un número aleatorio
